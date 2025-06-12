b7e91b5406f91bb0236d6b3d884028c1d3e9c9ed
jeongseohan93 <minsang580@gmail.com>
2025-06-12 20:48:59 +0900

    feat: 약식 로그인 및 JWT 기반 role별 페이지 전환 구현

---
+++ frontend/src/features/mainPage/LoginPage.jsx
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { loginUser } from "../../services/userApi";
+import { jwtDecode } from "jwt-decode";
+
+function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [pw, setPw] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const result = await loginUser({ email, password: pw });
+      if (result.success) {
+        
+        if (result.token) {
+          localStorage.setItem("token", result.token);
+        }
+      } else {
+        alert(result.message || "로그인 실패");
+      }
+      const decodToken = jwtDecode(result.token);
+      const userRole = decodToken.role;
+      if (userRole === "admin") {
+        navigate("/admin");
+      } else {
+        navigate("/");
+      }
+    } catch (err) {
+      alert("서버 오류");
+    }
+  };
+
+  function RegisterPage() {
+    navigate("/register");
+  };
+
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>로그인 페이지</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <input
+            type="email"
+            placeholder="이메일"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+            autoComplete="username"
+          />
+        </div>
+        <div>
+          <input
+            type="password"
+            placeholder="비밀번호"
+            value={pw}
+            onChange={e => setPw(e.target.value)}
+            autoComplete="current-password"
+          />
+        </div>
+        
+        <button type="submit">로그인</button>
+      </form>
+      <button
+          type="button"
+          onClick={ RegisterPage }
+        >회원가입</button>
+    </div>
+  );
+}
+
+export default LoginPage;
--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -1,8 +1,8 @@
 // 리액트 라우터에서 필요한 컴포넌트들 불러옴
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Main from './features/mainPage/MainPage'
-
-
+import Admin from './features/adminPage/DashBoard'
+import Login from './features/mainPage/LoginPage'
 // AppRoutes 컴포넌트: 앱의 모든 페이지 경로(라우팅)를 정의하는 곳
 function AppRoutes() {
   return (
@@ -10,6 +10,8 @@ function AppRoutes() {
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Main/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/admin' element={<Admin/>}/>
       </Routes>
     </BrowserRouter>
   );
