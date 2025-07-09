e589718025b541cac2752540f0f5df3a44db1795
한준희 <1702o505@naver.com>
2025-07-09 20:28:46 +0900

    feat: 250709 브랜치 병합

--- frontend/src/app/routes/adminRoutes.js
+++ frontend/src/app/routes/adminRoutes.js
@@ -1,14 +1,14 @@
 // src/app/routes/adminRoutes.js
 import { Outlet } from 'react-router-dom';
-import AdminOutlet from './AdminOutlet'
+import AdminOutlet from './adminOutlet';
 import DashboardPage from '@/features/admin/pages/DashBoardPage';
 import { PrivateRoute } from '@/shared';
-import UserManage from '@/features/admin/pages/userPage/userManage';
-import UserEdit from '@/features/admin/pages/userPage/userEdit';
-import ReportManage from '@/features/admin/pages/reportPage/reportManage';
-import ReportWrite from '@/features/admin/pages/reportPage/reportWrite';
-import ReportDetail from '@/features/admin/pages/reportPage/reportDetail';
-import ReportEdit from '@/features/admin/pages/reportPage/reportEdit';
+import UserManage from '@/features/admin/pages/userPage/UserManage.jsx';
+import UserEdit from '@/features/admin/pages/userPage/UserEdit.jsx';
+import ReportManage from '@/features/admin/pages/reportPage/ReportManage.jsx';
+import ReportWrite from '@/features/admin/pages/reportPage/ReportWrite.jsx';
+import ReportDetail from '@/features/admin/pages/reportPage/ReportDetail.jsx';
+import ReportEdit from '@/features/admin/pages/reportPage/ReportEdit.jsx';
 
 
 const adminRoutes = [
